07498131fe96c4610b48af54a80b5c149a50ff69
Evgeniy Shakhov <evgeniyshakhov@gmail.com>
2022-11-27 10:57:57 +0300

    first commit

---
+++ src/Strings.java
@@ -0,0 +1,18 @@
+public class Strings {
+    public static void main(String[] args) {
+
+        
+
+        boolean res = 4 > 9;
+        System.out.println("res = " + res);
+
+        int fInt = 1;
+        int sInt = 2;
+        System.out.println(fInt + sInt);
+
+        String f = "Hello";
+        String space = " ";
+        String s = "Java!";
+        System.out.println(f+space+s);
+    }
+}
---
+++ src/Variables.java
@@ -0,0 +1,6 @@
+public class Variables {
+    public static void main(String[] args) {
+        int myInt = 333;
+        System.out.println(myInt);
+    }
+}
